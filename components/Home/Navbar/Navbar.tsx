b8541debd28fcaa5a5af48d6af2698ae3a231c1d
marwancoder <marwane.ouarradi@gmail.com>
2025-05-09 18:26:52 +0100

    Added Shopping Bag in the Button

--- components/Home/Navbar/Navbar.tsx
+++ components/Home/Navbar/Navbar.tsx
@@ -57,7 +57,7 @@ const Navbar = ({openNav}: MobileNavProps) => {
                 <a href="#_" className="relative p-0.5 inline-flex items-center justify-center overflow-hidden group rounded-md">
                     <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
                     <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
-                        <span className="relative text-white">Buy Now</span>
+                        <span className="relative text-white flex items-center"> <FaShoppingBag className='text-white mr-2' /> Buy Now</span>
                     </span>
                 </a>
                 {/* Theme Switch Button */}
