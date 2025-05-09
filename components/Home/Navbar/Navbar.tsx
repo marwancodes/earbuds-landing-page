f38e4dfb617e1e8dfb29cb807ef17ec0596d6092
marwancoder <marwane.ouarradi@gmail.com>
2025-05-09 18:22:54 +0100

    Change Nav Button

--- components/Home/Navbar/Navbar.tsx
+++ components/Home/Navbar/Navbar.tsx
@@ -54,12 +54,10 @@ const Navbar = ({openNav}: MobileNavProps) => {
             {/* CTA BUTTON */}
             <div className='flex items-center space-x-4'>
                 {/* Buy Now Button */}
-                <a href="#_" className="box-border relative z-30 inline-flex items-center justify-center w-auto px-4 py-2 overflow-hidden font-bold text-white transition-all duration-300 bg-[#da1e37] rounded-md cursor-pointer group ring-offset-2 ring-1 ring-[#f8f8f8] ring-offset-[#ff86bd] hover:ring-offset-[#f72585] ease focus:outline-none">
-                    <span className="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
-                    <span className="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
-                    <span className="relative z-20 flex items-center text-sm">
-                        <FaShoppingBag className='text-white mr-2' />
-                        Button Text
+                <a href="#_" className="relative p-0.5 inline-flex items-center justify-center overflow-hidden group rounded-md">
+                    <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
+                    <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+                        <span className="relative text-white">Buy Now</span>
                     </span>
                 </a>
                 {/* Theme Switch Button */}
